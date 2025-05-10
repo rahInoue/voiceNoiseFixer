55b1229ecd5cacac3ebdb67e38a15d835ef321b6
rah inoue <sobata@alpaca-connect.co.jp>
2025-05-10 15:02:20 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="grid place-content-center h-48">
+      <div class="flex space-x-4">
+        <input type="text" class="input input-bordered w-full max-w-xs" placeholder="お名前を入力してください" />
+        <button class="btn btn-primary">入力</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script setup lang="ts">
+
+</script>
